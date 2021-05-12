7998064a6589588d3f8d94bf777dde28c5874e49
john <lord1229@gmail.com>
2021-05-13 00:52:43 +0900

    gift-chain에서 사용될 gift, giftbox, giftchian 구현

--- chain/blockchain.go
+++
@@ -1,8 +0,0 @@
-package chain
-
-type Block struct {
-}
-
-func CreateBlock() {
-
-}
---
+++ chain/giftchain.go
@@ -0,0 +1,49 @@
+package chain
+
+type Gift struct {
+	nickname      string
+	senderAddress string
+	content       string
+	startTime     int
+	endTime       int
+}
+
+type GiftBox struct {
+	gifts        []Gift
+	timestamp    int
+	nonce        string
+	previousHash string
+}
+
+type GiftChain struct {
+	chain     []GiftBox
+	neighbors []string
+	address   string
+}
+
+func CreateGift(nickname, senderAddress, content string, startTime, endTime int) *Gift {
+	return &Gift{
+		nickname:      nickname,
+		senderAddress: senderAddress,
+		content:       content,
+		startTime:     startTime,
+		endTime:       endTime,
+	}
+}
+
+func CreateGiftBox(gifts []Gift, timestamp int, nonce, previousHash string) *GiftBox {
+	return &GiftBox{
+		gifts:        gifts,
+		timestamp:    timestamp,
+		nonce:        nonce,
+		previousHash: previousHash,
+	}
+}
+
+func (g *GiftBox) AddGiftToBox(gift Gift) {
+	g.gifts = append(g.gifts, gift)
+}
+
+func (g *GiftChain) AddGifgBoxToChain(giftbox GiftBox) {
+	g.chain = append(g.chain, giftbox)
+}
