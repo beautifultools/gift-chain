4e2c1919e174ee21a84c7fcafb025d98fa91386f
john <lord1229@gmail.com>
2021-06-07 23:07:29 +0900

    GiftChain 만들고 외부에서 이웃 목록 가져오는 부분 구현

--- chain/giftchain.go
+++ chain/giftchain.go
@@ -1,11 +1,17 @@
 package chain
 
-type Gift struct {
-	Nickname      string
-	SenderAddress string
-	Content       string
-	StartTime     int64
-	EndTime       int64
+import (
+	"context"
+	"github.com/beautifultools/gift-chain/grpc/neighbor"
+	"google.golang.org/grpc"
+	"log"
+	"time"
+)
+
+type GiftChain struct {
+	chain     []GiftBox
+	neighbors []string
+	address   string
 }
 
 type GiftBox struct {
@@ -15,11 +21,44 @@ type GiftBox struct {
 	previousHash string
 }
 
-type GiftChain struct {
-	chain     []GiftBox
-	neighbors []string
-	address   string
+type Gift struct {
+	Nickname      string
+	SenderAddress string
+	Content       string
+	StartTime     int64
+	EndTime       int64
 }
+
+func CreateGiftChain(serverAddress string) *GiftChain{
+	getNeighborAddressesByGivenServer(serverAddress)
+
+	return &GiftChain{
+		chain:     []GiftBox{},
+		neighbors: []string{},
+	}
+}
+
+func getNeighborAddressesByGivenServer(serverAddress string) []string {
+	conn, err := grpc.Dial(serverAddress, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := neighbor.NewNeighborGreeterClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	neighbors, err := c.GetNeighbors(ctx, &neighbor.NeighborOptions{})
+
+	if err != nil {
+		log.Fatalf("fail to get neighbors: %v", err)
+	}
+	
+	return neighbors.Addresses
+}
+
 
 func CreateGiftBox(gifts []Gift, timestamp int64, nonce, previousHash string) *GiftBox {
 	return &GiftBox{
